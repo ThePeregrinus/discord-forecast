1a1ca9822414c12fc30571b5a44d97058cc5bc4e
Peregrinus <zakharov.mark22@gmail.com>
2024-07-19 04:13:14 +0300

    updates

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,7 +1,33 @@
 async function interactionCreateHandler(interaction){
+    if(!interaction.isChatInputCommand()){
+        return;
+    }
     const command = interaction.client.commands.get(interaction.commandName)
 
-    await command.execute(interaction)
+    if(!command){
+        return;
+    }
+
+    try{
+        await command.execute(interaction)
+        console.log(interaction.user.username, 'used command', interaction.commandName )
+    }
+    catch(e){
+        console.error(e)
+        if(interaction.replied || interaction.deferred){
+            await interaction.followUp({
+                content:'Error when execute coma',
+                ephemeral: true
+            });
+        } else{
+            await interaction.reply({
+                content:'Error when execute coma',
+                ephemeral: true
+            })
+        }
+    }
+
+
 }
 
 module.exports = {
